efe0363bcd15b4334a9f3dc088e41bfa3d3fcd88
boby bewa <bobybewa6@mail.com>
2021-06-19 10:27:08 +0700

    change port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const http = require('http').createServer(app)
 // const io = require('socket.io')(http)
 const cors = require('cors')
-const port = 8000
+const port = process.env.PORT || 8000
 
 const io = require("socket.io")(http, {
     cors: {
